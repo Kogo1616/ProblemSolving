d271c78954438c7b670cdeeec7f650369166405a
Saba <162018265+Kogo1616@users.noreply.github.com>
2025-02-22 18:37:30 +0400

    Time: 1026 ms (6.26%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,8 @@
+SELECT T.x, T.y, T.z,
+    CASE WHEN 
+        T.x + T.y > T.z AND
+        T.y + T.z > T.x AND
+        T.x + T.z > T.y
+    THEN 'Yes' 
+    ELSE 'No' END AS triangle 
+FROM Triangle T; 
