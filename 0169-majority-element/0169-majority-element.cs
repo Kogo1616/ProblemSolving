8c95bcd2b370568029838ae38eaf312ae10fa09b
Saba <162018265+Kogo1616@users.noreply.github.com>
2025-03-09 18:59:19 +0400

    Time: 18 ms (22.37%), Space: 51.7 MB (5.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,14 +1,13 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-            return nums
+        return nums
         .GroupBy(x => x)
         .Select(x => new
         {
             key = x.Key,
             value = x.Select(x => x)
         })
-        .OrderByDescending(x => x.value.Count())
-        .FirstOrDefault()
+        .MaxBy(x => x.value.Count())
         .value
         .FirstOrDefault();
     }
